c612161ca62944e17f0efbf1cf96d343133e918e
mobesheli <mohsen.amiribesheli@outlook.com>
2018-04-05 15:19:00 +0100

    Update customer.php

--- src/customer.php
+++ src/customer.php
@@ -2,4 +2,7 @@
 use \Psr\Http\Message\ServerRequestInterface as Request;
 use \Psr\Http\Message\ResponseInterface as Response;
 $app = new \Slim\App;
-$app->get ('api/customer', function(Request $request, Response $response)
+$app->get ('api/customer', function(Request $request, Response $response) {
+           echo 'Hiaaaa';
+});
+
