f5e9405baecb97a3b2d53c8b6976e62af75ae65b
mobesheli <mohsen.amiribesheli@outlook.com>
2018-04-05 15:22:26 +0100

    Update customer.php

--- src/customer.php
+++ src/customer.php
@@ -1,9 +1,9 @@
 <?php
 use \Psr\Http\Message\ServerRequestInterface as Request;
 use \Psr\Http\Message\ResponseInterface as Response;
+require '../vendor/autoload.php';
 $app = new \Slim\App;
-$app->get ('api/customer', function(Request $request, Response $response) {
-           echo 'Hiaaaa';
+$app->get('/api/customer', function (Request $request, Response $response) {
+    echo 'hello';
 });
 $app->run();
-
